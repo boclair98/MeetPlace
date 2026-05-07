fb07591b7e6369a8867c3ab22224851aea04c851
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-07 15:19:07 +0900

    feat: add application entrypoint

---
+++ src/main/java/com/boclair/meetplace/MeetPlaceApplication.java
@@ -0,0 +1,12 @@
+package com.boclair.meetplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetPlaceApplication.class, args);
+    }
+}
